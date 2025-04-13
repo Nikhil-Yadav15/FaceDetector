c8b973bde77b5529ecc3e3c8f6a1e52af43afd0e
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-04-14 00:18:22 +0530

    Update Navbar.js

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-      <nav className="bg-blue-600 p-4 text-white">
+      <nav className="bg-gradient-to-l from-purple-950 via-20% to-purple-900 p-4 text-white">
         <div className="container mx-auto">
           <h1 className="text-2xl font-bold">Face Detector</h1>
         </div>
