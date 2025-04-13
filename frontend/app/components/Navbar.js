e1cb642edac41bf3ec0c47cda5a8d5077e4ade17
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-04-14 00:07:44 +0530

    Update Navbar.js

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
       <nav className="bg-blue-600 p-4 text-white">
         <div className="container mx-auto">
-          <h1 className="text-2xl font-bold">Python App</h1>
+          <h1 className="text-2xl font-bold">Face Detector</h1>
         </div>
       </nav>
     );
